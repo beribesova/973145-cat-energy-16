00c13d25e5ebb55648a402425c092691f704c34d
Nastya <beribesova@yandex.ru>
2019-05-17 20:10:24 +0300

    До защиты

--- source/js/nav.js
+++ source/js/nav.js
@@ -2,18 +2,15 @@
 
   var navMain = document.querySelector('.navigation');
   var navToggle = document.querySelector('.navigation__toggle');
-  // var navImage = document.querySelector('.navigation__image');
 
   navMain.classList.remove('navigation--nojs');
 
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('navigation--opened')) {
-      // navImage.classList.remove('navigation__image--opened');
       navMain.classList.remove('navigation--opened');
       navMain.classList.add('navigation--closed');
     } else {
-      // navImage.classList.add('navigation__image--opened');
       navMain.classList.add('navigation--opened');
       navMain.classList.remove('navigation--closed');
     }
